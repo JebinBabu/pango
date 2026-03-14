c4bedebe29730c553682ae6db8bbe8db7fdea10c
Jebin <jebinbabuc@gmail.com>
2026-03-15 00:22:44 +0530

    finished download script

--- codes/download_files.py
+++ codes/download_files.py
@@ -1,22 +1,60 @@
 #!/usr/bin/env python3
 
-import subprocess
-import pandas as pd
+from tqdm import tqdm
+import requests
 import argparse
 parser = argparse.ArgumentParser(description="A script that downloads genome files from NCBI using the assembly accession numbers")
 
-parser.add_argument("--inp", type=str, help="NCBI assembly summary file")
-parser.add_argument("--out", type=str, help="Output folder")
+parser.add_argument("-i","--inp", type=str, help="Input file containing all RefSeq assembly accessions", required=True)
+parser.add_argument("-t","--type", type=str, choices=["nucl","prot"], help="Filetype to download from ncbi", default="nucl", required=True)
+parser.add_argument("-o","--out", type=str, help="Output folder", required=True)
 
 args = parser.parse_args()
 
+accessions = []
 
-df = pd.read_csv(args.inp, sep='\t')
+with open(args.inp, "r") as infile:
 
-df = df[df['Assembly Level'] == 'Complete Genome']
-df = df.drop_duplicates('Organism Infraspecific Names Strain')
+    gcf_file = infile.readlines()
 
-print(df.shape)
+    for line in gcf_file:
 
+        line = line.replace("\n","")
 
-cmd = "curl https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/009/045/GCF_000009045.1_ASM904v1/GCF_000009045.1_ASM904v1_protein.faa.gz -o GCF_000009045.1_ASM904v1_protein.faa.gz"
+        accessions.append(line)
+
+file_types = {"nucl":"_cds_from_genomic.fna.gz","prot":"_protein.faa.gz"}
+
+print(f"Downloading {len(accessions)} {args.type} files!")
+
+count_not_down = 0
+
+for acc in tqdm(accessions):
+
+    acc1 = acc[:3]
+    acc2 = acc[4:7]
+    acc3 = acc[7:10]
+    acc4 = acc[10:13]
+
+    new_filename = acc + file_types[args.type]
+
+    url = f"https://ftp.ncbi.nlm.nih.gov/genomes/all/{acc1}/{acc2}/{acc3}/{acc4}/{acc}/{new_filename}"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+
+        with open(args.out + new_filename,"wb") as new_file:
+
+            new_file.write(response.content)
+
+            # print(f"Downloaded {acc}{file_types[args.type]}")
+    else:
+        print(f"Couldn't download {acc}{file_types[args.type]}, please check the accession or download manually")
+
+        count_not_down += 1
+
+    if count_not_down > 0:
+
+        print(f"{count_not_down} files not downloaded!") 
+
---
+++ codes/test.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df = pd.read_csv('B_subtilis.tsv', delimiter='\t')
+
+df = df[df['Assembly Level'] == 'Complete Genome']
+df = df.drop_duplicates('Organism Infraspecific Names Strain')
+
+df['new_name'] = df['Assembly Accession'] + "_" + df['Assembly Name']
+
+for name in df['new_name']:
+
+    print(name)
