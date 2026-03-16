e5d61648aac9f36f11b31496da1d577cacce4f3c
Jebin <jebinbabuc@gmail.com>
2026-03-16 14:49:06 +0530

    finished filter blast

--- codes/filter_blast.py
+++ codes/filter_blast.py
@@ -2,18 +2,35 @@
 
 import logging
 import argparse
-import subprocess
+import pandas as pd
 import glob
 from tqdm import tqdm
 parser = argparse.ArgumentParser(description="A script that downloads genome files from NCBI using the assembly accession numbers")
 
 parser.add_argument("-i","--inp", type=str, help="Input folder containing input fasta files", required=True)
-parser.add_argument("-e","--ext", type=str, help="Input file extension", default=".fna")
-parser.add_argument("-b","--blast_type", type=str, choices=["nucl","prot"], help="Filetype to download from ncbi", default="nucl")
-parser.add_argument("-t","--threads", type=int, help="Number of threads", default=8)
+parser.add_argument("-e","--evalue", type=float, help="E-value cutoff", default=1e-5)
+parser.add_argument("-p","--pident", type=float, help="Percentage identity cutoff", default=75)
+parser.add_argument("-l","--len", type=float, help="Percentage length overlap cutoff", default=80)
 
 args = parser.parse_args()
 
 logging.basicConfig(filename='app.log', level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     filemode='a')
+
+
+files = glob.glob(args.inp + "*.out")
+
+for file in tqdm(files):
+
+    df = pd.read_csv(file, delimiter='\t',header=None)
+    df.columns = ['qseqid', 'sseqid', 'pident', 'length', 'qlen', 'evalue']
+
+    df1 = df[df['pident'] >= args.pident]
+    df1 = df1[df1['evalue'] <= args.evalue]
+    df1["len_cov"] = df1['length']/ df1['qlen']
+    df1 = df1[df1['len_cov'] >= args.len/100]
+
+
+    df1.to_csv(file.replace(".out","_filtered.csv"), index=None)
+
