c3759404ab9121795a671cb8b66ea9fbf9352fc3
Jebin <jebinbabuc@gmail.com>
2026-03-26 19:31:57 +0530

    rename pango

---
+++ codes/pango.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python3
+
+import logging
+import argparse
+import pandas as pd
+from tqdm import tqdm
+import os
+parser = argparse.ArgumentParser(description="A script that downloads genome files from NCBI using the assembly accession numbers")
+
+parser.add_argument("-i","--inp", type=str, help="Input folder containing input fasta files", required=True)
+parser.add_argument("-cp","--core_perc", type=int, help="Relaxed cutoff for core genome", default = 100)
+parser.add_argument("-g","--genomes", type=str, help="Custom genomes list to generate pangenome of")
+parser.add_argument("-o","--out", type=str, help="Output folder", default="../out/")
+parser.add_argument("--coregenome", action="store_true", help="Only generate coregenome")
+parser.add_argument('-p',"--pan", type=str, help="Path to pangenome if skip_pan is used")
+
+
+args = parser.parse_args()
+
+logging.basicConfig(filename='app.log', level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    filemode='a')
+
+
+def generate_coregenome(protein_families, genome_list, core_perc, outfolder):
+
+    with open(f"{outfolder}coregenome_{core_perc}%.csv", 'w') as core_file:
+
+        new_str = ",".join(genome_list)
+
+        core_file.write(new_str)
+        core_file.write('\n')
+        core_count = 0
+
+        for family in protein_families:
+
+            family = [i for i in family if str(i) != 'nan']
+            familiy_genomes = list(map(lambda x: x.split(';')[0], family))
+
+            if len(set(familiy_genomes)) < len(familiy_genomes):
+                continue
+
+            if len(set(familiy_genomes))/len(genome_list) < core_perc/100:
+
+                continue
+            else:
+
+                new_family = ["" for i in genome_list]
+
+                for genome_index, genome in enumerate(genome_list):
+                    for protein_id in family:
+
+                        if genome in protein_id:
+
+                            new_family[genome_index] = protein_id
+                            break
+
+            
+                new_str = ",".join(new_family)
+
+                core_file.write(new_str)
+                core_file.write('\n')
+                core_count += 1
+
+        logging.info(f"Found {core_count} protein families in the core genome")
+    
+
+
+try:
+    os.mkdir('../temp/pre_pangenomes/')
+except:
+    True
+
+if args.out == "../out/":
+    try:
+        os.mkdir('../out/')
+    except:
+        True
+
+if args.coregenome:
+    if not args.pan:
+
+        print("Use -p or --pan to choose the pangenome file to generate coregenome")
+        quit()
+
+# Generating pre-pan genomes
+
+df_all_proteins = pd.read_csv('../temp/all_proteins.txt', header=None)[['protein_new_name']]
+df_all_proteins.columns = [0]
+df_all_proteins[1] = df_all_proteins[0].apply(lambda x: x.split(';')[0])
+
+if args.genomes:
+    custom_genomes_list = pd.read_csv(args.genomes, header=None)[0].values.tolist()
+    all_genomes_list = custom_genomes_list.copy()
+    df_all_proteins = df_all_proteins[df_all_proteins[1].isin(custom_genomes_list)]
+
+
+
+else:
+    all_genomes_list = pd.read_csv('../temp/all_genomes.txt', header=None)[0].values.tolist()
+    df_all_proteins = df_all_proteins[df_all_proteins[1].isin(all_genomes_list)]
+    
+    
+if not args.coregenome:
+    
+    for gcf1 in tqdm(all_genomes_list):
+
+        pre_pangenome = pd.DataFrame()
+
+        for gcf2 in all_genomes_list:
+
+            if gcf1 == gcf2: 
+                continue
+
+            df_blast1 = pd.read_csv(f'{args.inp}{gcf1}_vs_{gcf2}_filtered.csv')[['qseqid','sseqid']]
+            df_blast1.columns = [gcf1, gcf2]
+            df_blast2 = pd.read_csv(f'{args.inp}{gcf2}_vs_{gcf1}_filtered.csv')[['qseqid','sseqid']]
+            df_blast2.columns = [gcf2, gcf1]
+
+            df_overlap = pd.merge(df_blast1, df_blast2, on=[gcf1, gcf2], how='inner')
+
+            # Finding and removing proteins with multpiple blast hits.
+
+            df_duplicated = df_overlap[df_overlap[gcf1].duplicated()]
+            df_final = df_overlap[~df_overlap[gcf1].isin(df_duplicated[gcf1])]
+
+            # Removing singletons from all proteins list to add the remaining to the final pangenome. 
+
+            df_all_proteins = df_all_proteins[~df_all_proteins[0].isin(df_final[gcf1])]
+            df_all_proteins = df_all_proteins[~df_all_proteins[0].isin(df_final[gcf2])]
+            
+            if pre_pangenome.shape[1] == 0:
+
+                pre_pangenome = df_final.copy()
+
+            else:
+
+                pre_pangenome = pd.merge(pre_pangenome, df_final, on=gcf1, how="left")
+
+        pre_pangenome = pre_pangenome[all_genomes_list]
+        pre_pangenome.to_csv(f'../temp/pre_pangenomes/{gcf1}.csv', index=None)
+
+
+
+# Compiling pre-pangenomes
+
+if not args.coregenome:
+
+    df_pre_pangenome_final = pd.DataFrame()
+
+    for gcf in tqdm(all_genomes_list):
+
+        df_pre_pangenome = pd.read_csv(f"../temp/pre_pangenomes/{gcf}.csv",dtype=str)
+
+        if df_pre_pangenome_final.shape[0] == 0:
+
+            df_pre_pangenome_final = df_pre_pangenome.copy()
+
+        else:
+
+            df_pre_pangenome_final = pd.merge(df_pre_pangenome_final, df_pre_pangenome, on=all_genomes_list, how='outer', validate='1:1')
+
+
+    df_pre_pangenome_final_list = df_pre_pangenome_final.values.tolist()
+
+    protein_families = []
+
+
+    for new_families in tqdm(df_pre_pangenome_final_list):
+
+        new_families_set = set(new_families)
+
+        is_new_family = True
+
+
+        for fam_index, families in enumerate(protein_families):
+
+            families_set = set(families)
+
+            if len(new_families_set.intersection(families_set)) >= 2:
+
+                is_new_family = False
+                protein_families[fam_index] = list(new_families_set.union(families_set))
+
+                break
+
+
+        if is_new_family:
+
+            protein_families.append(new_families)
+
+    df_protein_families = pd.DataFrame(protein_families,columns=None)
+    df_protein_families = pd.concat([df_protein_families, df_all_proteins[[0]]], axis=0)
+
+    df_protein_families.to_csv(f'{args.out}pangenome.csv',index=None, header=None)
+
+logging.info(f"Ran pango on {len(all_genomes_list)} genomes")
+
+
+
+if not args.coregenome:
+    protein_families = df_protein_families.values.tolist()
+
+else:
+    df_pangenome = pd.read_csv(args.pan, header=None,dtype=str)
+    protein_families = df_pangenome.values.tolist()
+
+logging.info(f"Found {len(protein_families)} protein families in the pan genome")
+
+
+generate_coregenome(protein_families, all_genomes_list, args.core_perc, args.out)
